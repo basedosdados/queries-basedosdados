f8fb42abb987f01d41ef02facd240c173e90ec65
lucascr91 <lucas.ecomg@gmail.com>
2022-05-05 17:18:32 -0300

    [infra] update metricas_tweets model

--- models/br_bd_indicadores/metricas_tweets.sql
+++ models/br_bd_indicadores/metricas_tweets.sql
@@ -1,4 +1,7 @@
-SELECT 
+SELECT
+SAGE_CAST(upload_day AS DATE) upload_day,
+SAFE_CAST(id AS STRING) id,
+SAFE_CAST(text AS STRING) text,
 SAFE_CAST(retweet_count AS INT64) retweet_count,
 SAFE_CAST(reply_count AS INT64) reply_count,
 SAFE_CAST(like_count AS INT64) like_count,
@@ -7,4 +10,8 @@ SAFE_CAST(created_at AS STRING) created_at,
 SAFE_CAST(url_link_clicks AS FLOAT64) url_link_clicks,
 SAFE_CAST(user_profile_clicks AS FLOAT64) user_profile_clicks,
 SAFE_CAST(impression_count AS FLOAT64) impression_count
+SAFE_CAST(quote_count AS INT64) following_count,
+SAFE_CAST(quote_count AS INT64) followers_count,
+SAFE_CAST(quote_count AS INT64) tweet_count,
+SAFE_CAST(quote_count AS INT64) listed_count,
 FROM `basedosdados-dev.br_bd_indicadores_staging.metricas_tweets`
