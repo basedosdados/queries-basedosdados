84697b45b6c0d45936c7f698cf7a9e9c5523ac2b
Lucas Cavalcanti Rodrigues <lucas.ecomg@gmail.com>
2022-05-06 13:18:23 -0300

    Update metricas_tweets.sql

--- models/br_bd_indicadores/metricas_tweets.sql
+++ models/br_bd_indicadores/metricas_tweets.sql
@@ -2,7 +2,7 @@ SELECT
 SAFE_CAST(upload_day AS DATE) upload_day,
 SAFE_CAST(id AS STRING) id,
 SAFE_CAST(text AS STRING) text,
-SAFE_CAST(created_at AS STRING) created_at,
+SAFE_CAST(created_at AS DATE '%Y-%m-%dT%H:%I:%S.00Z') created_at,
 SAFE_CAST(retweet_count AS INT64) retweet_count,
 SAFE_CAST(reply_count AS INT64) reply_count,
 SAFE_CAST(like_count AS INT64) like_count,
@@ -10,8 +10,8 @@ SAFE_CAST(quote_count AS INT64) quote_count,
 SAFE_CAST(impression_count AS FLOAT64) impression_count,
 SAFE_CAST(user_profile_clicks AS FLOAT64) user_profile_clicks,
 SAFE_CAST(url_link_clicks AS FLOAT64) url_link_clicks,
-SAFE_CAST(quote_count AS INT64) following_count,
-SAFE_CAST(quote_count AS INT64) followers_count,
-SAFE_CAST(quote_count AS INT64) tweet_count,
-SAFE_CAST(quote_count AS INT64) listed_count
+SAFE_CAST(following_count AS INT64) following_count,
+SAFE_CAST(followers_count AS INT64) followers_count,
+SAFE_CAST(tweet_count AS INT64) tweet_count,
+SAFE_CAST(listed_count AS INT64) listed_count
 FROM `basedosdados-dev.br_bd_indicadores_staging.metricas_tweets`
