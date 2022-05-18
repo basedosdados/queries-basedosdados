200cf39d428556301e6dfd8cf40572f2b7c08777
lucascr91 <lucas.ecomg@gmail.com>
2022-05-17 21:21:49 -0300

    [infra] add queries for existing pipelines

---
+++ models/br_cvm_administradores_carteira/pessoa_fisica.sql
@@ -0,0 +1,9 @@
+SELECT 
+SAFE_CAST(nome AS STRING) nome,
+SAFE_CAST(data_registro AS DATE) data_registro,
+SAFE_CAST(data_cancelamento AS DATE) data_cancelamento,
+SAFE_CAST(motivo_cancelamento AS STRING) motivo_cancelamento,
+SAFE_CAST(situacao AS STRING) situacao,
+SAFE_CAST(data_inicio_situacao AS DATE) data_inicio_situacao,
+SAFE_CAST(categoria_registro AS STRING) categoria_registro
+FROM basedosdados-staging.br_cvm_administradores_carteira_staging.pessoa_fisica AS t
---
+++ models/br_ibge_ipca/mes_categoria_municipio.sql
@@ -0,0 +1,12 @@
+SELECT 
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(mes AS INT64) mes,
+SAFE_CAST(id_municipio AS STRING) id_municipio,
+SAFE_CAST(id_categoria AS STRING) id_categoria,
+SAFE_CAST(id_categoria_bd AS STRING) id_categoria_bd,
+SAFE_CAST(categoria AS STRING) categoria,
+SAFE_CAST(peso_mensal AS FLOAT64) peso_mensal,
+SAFE_CAST(variacao_mensal AS FLOAT64) variacao_mensal,
+SAFE_CAST(variacao_anual AS FLOAT64) variacao_anual,
+SAFE_CAST(variacao_doze_meses AS FLOAT64) variacao_doze_meses
+FROM basedosdados-staging.br_ibge_ipca_staging.mes_categoria_municipio AS t
