ca09876743175850bd4f0e0616673cc31703b6c2
lucascr91 <lucas.ecomg@gmail.com>
2022-05-18 15:46:41 -0300

    [infra] add schema for cgu, cvm and inpc

---
+++ gists/schema_as_yml.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Gera um yaml com o schema da tabela
+
+# Para rodar esse script o usuário deve rodar:
+# bash schema_as_yml.sh [dataset] [table]
+# fazendo as substituições pertinentes
+
+dataset=$1
+table=$2
+
+bq show --schema basedosdados:$dataset.$table  | jq '.' | yq -P | egrep -v type
