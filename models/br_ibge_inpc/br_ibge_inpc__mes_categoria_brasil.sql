6869ebf9e9d0c5ff5f21e79c521821228a3eb261
Lucas Cavalcanti Rodrigues <lucas.ecomg@gmail.com>
2022-05-24 17:13:44 -0300

    Update br_ibge_inpc__mes_categoria_brasil.sql

--- models/br_ibge_inpc/br_ibge_inpc__mes_categoria_brasil.sql
+++ models/br_ibge_inpc/br_ibge_inpc__mes_categoria_brasil.sql
@@ -1,4 +1,4 @@
-{{ config(alias='mes_brasil', schema='br_ibge_inpc') }}
+{{ config(alias='mes_categoria_brasil', schema='br_ibge_inpc') }}
 SELECT 
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
