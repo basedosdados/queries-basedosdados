5458fc8002866220ae9deeba3cf3fc474c6e7e36
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2022-09-12 17:10:27 -0300

    correct data types in sql files

--- models/br_fgv_igp/igp_og_mensal.sql
+++ models/br_fgv_igp/igp_og_mensal.sql
@@ -1,9 +1,9 @@
 SELECT
-SAFE_CAST(ano AS STRING) ano,
-SAFE_CAST(mes AS STRING) mes,
-SAFE_CAST(indice AS STRING) indice,
-SAFE_CAST(var_mensal AS STRING) var_mensal,
-SAFE_CAST(var_12_meses AS STRING) var_12_meses,
-SAFE_CAST(acum_ano AS STRING) acum_ano,
-SAFE_CAST(indice_fechamento_mensal AS STRING) indice_fechamento_mensal
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(mes AS INT64) mes,
+SAFE_CAST(indice AS FLOAT64) indice,
+SAFE_CAST(var_mensal AS FLOAT64) var_mensal,
+SAFE_CAST(var_12_meses AS FLOAT64) var_12_meses,
+SAFE_CAST(acum_ano AS FLOAT64) acum_ano,
+SAFE_CAST(indice_fechamento_mensal AS FLOAT64) indice_fechamento_mensal
 FROM basedosdados-dev.br_fgv_igp_staging.igp_og_mes AS t
