d0119348bde8c4d28731ea17a08a63eb13c1152d
Ricardo Dahis <rdahis@gmail.com>
2022-09-20 20:47:23 -0400

    fix: table id to name

--- models/br_bd_metadados/tables.sql
+++ models/br_bd_metadados/tables.sql
@@ -1,7 +1,7 @@
 SELECT
   SAFE_CAST(dataset_id AS STRING) dataset_id,
   SAFE_CAST(id AS STRING) id,
-  SAFE_CAST(table_id AS STRING) table_id,
+  SAFE_CAST(name AS STRING) name,
   SAFE_CAST(date_created AS DATE) date_created,
   SAFE_CAST(date_last_modified AS DATE) date_last_modified,
   SAFE_CAST(spatial_coverage AS STRING) spatial_coverage,
