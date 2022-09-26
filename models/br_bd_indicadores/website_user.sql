3cfe60f75b0f62af8a99e4bccaa5f8e41cfaaa04
lucascr91 <lucas.ecomg@gmail.com>
2022-09-26 20:37:43 -0300

    [infra] website user to prod

--- models/br_bd_indicadores/website_user.sql
+++ models/br_bd_indicadores/website_user.sql
@@ -6,4 +6,4 @@ SAFE_CAST(users_14_days AS INT64) users_14_days,
 SAFE_CAST(users_28_days AS INT64) users_28_days,
 SAFE_CAST(users_30_days AS INT64) users_30_days,
 SAFE_CAST(new_users AS INT64) new_users
-FROM basedosdados-dev.br_bd_indicadores_staging.website_user AS t
+FROM basedosdados-staging.br_bd_indicadores_staging.website_user AS t
