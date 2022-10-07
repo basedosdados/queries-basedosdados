b673d72de6a4c6cfbd1cc8d813b16c1592479666
d116626 <d116626@gmail.com>
2022-10-07 00:00:38 -0300

    feat: add br_tse_eleicoes_2022

---
+++ models/br_tse_eleicoes_2022/resultado_boletim_urna.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized='incremental',
+        partition_by={
+            "field": "sigla_uf",
+            "data_type": "string",
+        }    
+    )
+}}
+
+SELECT 
+  *
+FROM basedosdados-dev.br_tse_eleicoes_2022_staging.resultado_boletim_urna AS t
