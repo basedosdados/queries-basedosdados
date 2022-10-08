12e74819c0de804c70f01676ca44b8d2703525f4
d116626 <d116626@gmail.com>
2022-10-08 20:30:25 -0300

    feat: add local_votacao

---
+++ models/br_tse_eleicoes_2022/local_votacao.sql
@@ -0,0 +1,4 @@
+
+SELECT 
+  *
+FROM basedosdados-dev.br_tse_eleicoes_2022_staging.local_votacao AS t
--- models/br_tse_eleicoes_2022/resultado_boletim_urna.sql
+++ models/br_tse_eleicoes_2022/resultado_boletim_urna.sql
@@ -1,13 +1,3 @@
-{{
-    config(
-        materialized='incremental',
-        partition_by={
-            "field": "sigla_uf",
-            "data_type": "string",
-        }    
-    )
-}}
-
 SELECT 
   *
 FROM basedosdados-dev.br_tse_eleicoes_2022_staging.resultado_boletim_urna AS t
