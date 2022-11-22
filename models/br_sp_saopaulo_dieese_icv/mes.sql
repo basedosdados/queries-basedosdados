81d2622adf5551457c80351183b6254ed5f387ae
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2022-11-22 18:37:52 -0300

    adding fields in br_sp_saopaulo_dieese_icv

--- models/br_sp_saopaulo_dieese_icv/mes.sql
+++ models/br_sp_saopaulo_dieese_icv/mes.sql
@@ -1,1 +1,6 @@
-SELECT * FROM `basedosdados-staging.br_sp_saopaulo_dieese_icv_staging.mes`
+SELECT
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(mes AS INT64) mes,
+SAFE_CAST(indice AS FLOAT64) indice,
+SAFE_CAST(variacao_mensal AS FLOAT64) variacao_mensal
+FROM basedosdados-staging.br_sp_saopaulo_dieese_icv_staging.mes as t
