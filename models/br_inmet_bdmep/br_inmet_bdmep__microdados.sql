991031e0b4d3b1ea22d91dab25e25f49c32a870a
Lucas Cavalcanti Rodrigues <lucas.ecomg@gmail.com>
2023-04-11 15:01:59 -0300

    Update br_inmet_bdmep__microdados.sql

--- models/br_inmet_bdmep/br_inmet_bdmep__microdados.sql
+++ models/br_inmet_bdmep/br_inmet_bdmep__microdados.sql
@@ -21,4 +21,4 @@ SAFE_CAST(umidade_rel_hora AS FLOAT64) umidade_rel_hora,
 SAFE_CAST(vento_direcao AS FLOAT64) vento_direcao,
 SAFE_CAST(vento_rajada_max AS FLOAT64) vento_rajada_max,
 SAFE_CAST(vento_velocidade AS FLOAT64) vento_velocidade
-FROM basedosdados-dev.br_inmet_bdmep_staging.microdados AS t
+FROM basedosdados-staging.br_inmet_bdmep_staging.microdados AS t
