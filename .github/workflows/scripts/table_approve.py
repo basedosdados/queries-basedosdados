c20846aa4095cb7e09368494f0aa8d82eae2b4d0
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-05-19 15:46:39 -0300

    feat: initial tb approve script

--- .github/workflows/scripts/table_approve.py
+++ .github/workflows/scripts/table_approve.py
@@ -1,7 +1,141 @@
 from argparse import ArgumentParser
+import sys
+import traceback
+
+import basedosdados as bd
+from basedosdados import Dataset, Storage
 
 from backend import Backend
 from utils import expand_alls, get_datasets_tables_from_modified_files
+
+
+def push_table_to_bq(
+    dataset_id,
+    table_id,
+    source_bucket_name="basedosdados-dev",
+    destination_bucket_name="basedosdados",
+    backup_bucket_name="basedosdados-backup",
+):
+    # copy proprosed data between storage buckets
+    # create a backup of old data, then delete it and copies new data into the destination bucket
+    modes = ["staging", "raw", "auxiliary_files", "architecture", "header"]
+
+    for mode in modes:
+        try:
+            sync_bucket(
+                source_bucket_name=source_bucket_name,
+                dataset_id=dataset_id,
+                table_id=table_id,
+                destination_bucket_name=destination_bucket_name,
+                backup_bucket_name=backup_bucket_name,
+                mode=mode,
+            )
+            print()
+        except Exception as error:
+            print(f"DATA ERROR ON {mode}.{dataset_id}.{table_id}")
+            traceback.print_exc(file=sys.stderr)
+            print()
+
+    # create table object of selected table and dataset ID
+    tb = bd.Table(dataset_id=dataset_id, table_id=table_id)
+
+    # delete table from staging and prod if exists
+    tb.delete("all")
+
+    # create the staging table in bigquery
+    tb.create(
+        path=None,
+        if_table_exists="replace",
+        if_storage_data_exists="pass",
+        if_table_config_exists="pass",
+        dataset_is_public=True,
+    )
+
+    # updates the dataset description
+    Dataset(dataset_id).update(mode="prod")
+
+
+def sync_bucket(
+    source_bucket_name: str,
+    dataset_id: str,
+    table_id: str,
+    destination_bucket_name: str,
+    backup_bucket_name: str,
+    mode: str = "staging",
+) -> None:
+    """Copies proprosed data between storage buckets.
+    Creates a backup of old data, then delete it and copies new data into the destination bucket.
+
+    Args:
+        source_bucket_name (str):
+            The bucket name from which to copy data.
+        dataset_id (str):
+            Dataset id available in basedosdados. It should always come with table_id.
+        table_id (str):
+            Table id available in basedosdados.dataset_id.
+            It should always come with dataset_id.
+        destination_bucket_name (str):
+            The bucket name which data will be copied to.
+            If None, defaults to the bucket initialized when instantianting Storage object
+            (check it with the Storage.bucket proprerty)
+        backup_bucket_name (str):
+            The bucket name for where backup data will be stored.
+        mode (str): Optional
+            Folder of which dataset to update.[raw|staging|header|auxiliary_files|architecture]
+
+    Raises:
+        ValueError:
+            If there are no files corresponding to the given dataset_id and table_id on the source bucket
+    """
+
+    ref = Storage(dataset_id=dataset_id, table_id=table_id)
+
+    prefix = f"{mode}/{dataset_id}/{table_id}/"
+
+    source_ref = (
+        ref.client["storage_staging"]
+        .bucket(source_bucket_name)
+        .list_blobs(prefix=prefix)
+    )
+
+    destination_ref = ref.bucket.list_blobs(prefix=prefix)
+
+    if len(list(source_ref)) == 0:
+        raise ValueError(
+            f"No objects found on the source bucket {source_bucket_name}.{prefix}"
+        )
+
+    if len(list(destination_ref)):
+        backup_bucket_blobs = list(
+            ref.client["storage_staging"]
+            .bucket(backup_bucket_name)
+            .list_blobs(prefix=prefix)
+        )
+        if len(backup_bucket_blobs):
+            print(f"{mode.upper()}: DELETE BACKUP DATA")
+            ref.delete_table(
+                not_found_ok=True, mode=mode, bucket_name=backup_bucket_name
+            )
+
+        print(f"{mode.upper()}: BACKUP OLD DATA")
+        ref.copy_table(
+            source_bucket_name=destination_bucket_name,
+            destination_bucket_name=backup_bucket_name,
+            mode=mode,
+        )
+
+        print(f"{mode.upper()}: DELETE OLD DATA")
+        ref.delete_table(
+            not_found_ok=True, mode=mode, bucket_name=destination_bucket_name
+        )
+
+    print(f"{mode.upper()}: TRANSFER NEW DATA")
+    ref.copy_table(
+        source_bucket_name=source_bucket_name,
+        destination_bucket_name=destination_bucket_name,
+        mode=mode,
+    )
+
 
 if __name__ == "__main__":
     # Start argument parser
@@ -51,3 +185,15 @@ if __name__ == "__main__":
             expand_alls(dataset_id, table_id, backend)
         )
     existing_datasets_tables = expanded_existing_datasets_tables
+
+    # Sync and create tables
+    for dataset_id, table_id in existing_datasets_tables:
+        print(f"Creating table {dataset_id}.{table_id}...")
+        push_table_to_bq(
+            dataset_id=dataset_id,
+            table_id=table_id,
+            source_bucket_name="basedosdados-dev",  # TODO: replace
+            destination_bucket_name="basedosdados",  # TODO: replace
+            backup_bucket_name="basedosdados-backup",  # TODO: replace
+        )
+        print(f"Table {dataset_id}.{table_id} created.")
