85597d30d810e83523fe344fa2ae3297eeb34e0e
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-05-22 15:51:49 -0300

    chore: modify sql

--- models/test_dataset/test_table.sql
+++ models/test_dataset/test_table.sql
@@ -2,5 +2,5 @@ SELECT
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
 SAFE_CAST(sigla_uf AS STRING) sigla_uf,
-SAFE_CAST(dado AS STRING) dado
+SAFE_CAST(dado AS STRING) dadoo
 FROM basedosdados-staging.test_dataset_staging.test_table AS t
