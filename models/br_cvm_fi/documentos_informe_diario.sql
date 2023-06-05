bbf300ede07800ffd5a6cb8809fd338fe3240af2
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-06-05 10:33:15 -0300

    fix: fix typo error

--- models/br_cvm_fi/documentos_informe_diario.sql
+++ models/br_cvm_fi/documentos_informe_diario.sql
@@ -23,7 +23,7 @@ SAFE_CAST(valor_total AS FLOAT64) valor_total,
 SAFE_CAST(valor_cota AS FLOAT64) valor_cota,
 SAFE_CAST(valor_patrimonio_liquido AS FLOAT64) valor_patrimonio_liquido,
 SAFE_CAST(captacao_dia AS FLOAT64) captacao_dia,
-SAFE_CAST(regate_dia AS FLOAT64) regate_dia,
+SAFE_CAST(regate_dia AS FLOAT64) resgate_dia,
 SAFE_CAST(quantidade_cotistas AS INT64) quantidade_cotistas,
 FROM basedosdados-staging.br_cvm_fi_staging.documentos_informe_diario AS t
 
