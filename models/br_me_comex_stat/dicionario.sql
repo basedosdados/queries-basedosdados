92b754b66432f3dadff4e259c539ebb131497659
folhesgabriel <pisagabriel09@gmail.com>
2023-06-06 11:09:56 -0300

    add br_me_comex_stat dicionario dataset to production zone

---
+++ models/br_me_comex_stat/dicionario.sql
@@ -0,0 +1,7 @@
+SELECT 
+SAFE_CAST(id_tabela AS STRING) id_tabela,
+SAFE_CAST(coluna AS STRING) coluna,
+SAFE_CAST(chave AS STRING) chave,
+SAFE_CAST(cobertura_temporal AS STRING) cobertura_temporal,
+SAFE_CAST(valor AS STRING) valor
+FROM basedosdados-staging.br_me_comex_stat_staging.dicionario AS t
