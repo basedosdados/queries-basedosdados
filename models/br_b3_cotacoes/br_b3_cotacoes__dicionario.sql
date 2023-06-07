b9414fa1ddc46f8a20eca097899bb4dedf6d68d0
tricktx <patrick.tx@hotmail.com.br>
2023-06-07 11:44:04 -0300

    feat: dicionario b3

---
+++ models/br_b3_cotacoes/br_b3_cotacoes__dicionario.sql
@@ -0,0 +1,10 @@
+{{ config(alias='dicionario', schema='br_b3_cotacoes') }}
+
+
+SELECT 
+SAFE_CAST(id_tabela AS STRING) id_tabela,
+SAFE_CAST(coluna AS STRING) coluna,
+SAFE_CAST(chave AS STRING) chave,
+SAFE_CAST(cobertura_temporal AS STRING) cobertura_temporal,
+SAFE_CAST(valor AS STRING) valor
+FROM basedosdados-staging.br_b3_cotacoes_staging.dicionario AS t
