f25bb222bcf59d2056097efb4d5d20e87f756381
tricktx <patrick.tx@hotmail.com.br>
2023-06-16 11:02:38 -0300

    fix: dev

--- models/br_b3_cotacoes/br_b3_cotacoes__dicionario.sql
+++ models/br_b3_cotacoes/br_b3_cotacoes__dicionario.sql
@@ -8,4 +8,4 @@ SAFE_CAST(chave AS STRING) chave,
 SAFE_CAST(cobertura_temporal AS STRING) cobertura_temporal,
 SAFE_CAST(valor AS STRING) valor
 
-FROM basedosdados-staging.br_b3_cotacoes_staging.dicionario AS t
+FROM basedosdados-dev.br_b3_cotacoes_staging.dicionario AS t 
