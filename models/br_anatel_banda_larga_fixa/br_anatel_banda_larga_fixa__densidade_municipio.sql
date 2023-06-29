894a279f08ebfb43fc801d531e23cb308419a44e
tricktx <patrick.tx@hotmail.com.br>
2023-06-29 10:04:59 -0300

    fix: atualizando dbt

--- models/br_anatel_banda_larga_fixa/br_anatel_banda_larga_fixa__densidade_municipio.sql
+++ models/br_anatel_banda_larga_fixa/br_anatel_banda_larga_fixa__densidade_municipio.sql
@@ -5,6 +5,6 @@ SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
 SAFE_CAST(sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(id_municipio AS STRING) id_municipio,
-SAFE_CAST(densidade AS FLOAT64) densidade,
+SAFE_CAST(densidade AS FLOAT64) densidade
 
 FROM basedosdados-staging.br_anatel_banda_larga_fixa_staging.densidade_municipio AS t
