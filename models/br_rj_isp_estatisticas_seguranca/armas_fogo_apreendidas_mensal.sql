8c5915dcf017a7fb9b6e5a7ac279910ee8a2766d
Patrick Teixeira <105399231+tricktx@users.noreply.github.com>
2023-07-18 11:57:42 -0300

    Update armas_fogo_apreendidas_mensal.sql

--- models/br_rj_isp_estatisticas_seguranca/armas_fogo_apreendidas_mensal.sql
+++ models/br_rj_isp_estatisticas_seguranca/armas_fogo_apreendidas_mensal.sql
@@ -7,4 +7,4 @@ SAFE_CAST(id_aisp AS STRING) id_aisp,
 SAFE_CAST(id_risp AS STRING) id_risp,
 SAFE_CAST(quantidade_arma_fogo_apreendida AS INT64) quantidade_arma_fogo_apreendida
 
-FROM basedosdados-dev.br_rj_isp_estatisticas_seguranca_staging.armas_fogo_apreendidas_mensal AS t
+FROM basedosdados-staging.br_rj_isp_estatisticas_seguranca_staging.armas_fogo_apreendidas_mensal AS t
