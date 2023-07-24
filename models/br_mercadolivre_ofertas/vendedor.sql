0954bef00ed213b490d5d078c710d04bc82398e4
Gabrielle Carvalho <77730866+gabrielle-carv@users.noreply.github.com>
2023-07-24 15:34:55 -0300

    att

--- models/br_mercadolivre_ofertas/vendedor.sql
+++ models/br_mercadolivre_ofertas/vendedor.sql
@@ -30,7 +30,7 @@ WITH main AS (
   from `basedosdados-staging.br_mercadolivre_ofertas_staging.vendedor`
 ), tabela_ordenada AS (
 SELECT 
-  PARSE_DATE('%Y-%m-%d', dia) AS data_consulta,
+  dia AS data_consulta,
   id_municipio,
   main.id_vendedor,
   nome AS vendedor,
