b66a5f214b027ed5563593d02cfef227cc5c7a97
Patrick Teixeira <105399231+tricktx@users.noreply.github.com>
2023-07-31 08:39:42 -0300

    Update br_anatel_telefonia_movel__densidade_brasil_atualizado.sql

--- models/br_anatel_telefonia_movel/br_anatel_telefonia_movel__densidade_brasil_atualizado.sql
+++ models/br_anatel_telefonia_movel/br_anatel_telefonia_movel__densidade_brasil_atualizado.sql
@@ -8,4 +8,3 @@ SAFE_CAST(mes AS INT64) mes,
 SAFE_CAST(densidade AS FLOAT64) densidade
 
 FROM basedosdados-staging.br_anatel_telefonia_movel_staging.br_anatel_telefonia_movel_staging.densidade_brasil AS t
-
