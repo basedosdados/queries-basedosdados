a4f0520712ea52b8e6df369ed76a74cdd4b6c4f1
Patrick Teixeira <105399231+tricktx@users.noreply.github.com>
2023-08-11 13:41:50 -0300

    Update br_anp_precos_combustiveis__microdados.sql

--- models/br_anp_precos_combustiveis/br_anp_precos_combustiveis__microdados.sql
+++ models/br_anp_precos_combustiveis/br_anp_precos_combustiveis__microdados.sql
@@ -3,25 +3,29 @@
     schema='br_anp_precos_combustiveis',
     materialized='table',
     partition_by={
-      "field": "data_coleta",
-      "data_type": "date",
-      "granularity": "day"
+      "field": "ano",
+      "data_type": "int64",
+      "range": {
+        "start": 2004,
+        "end": 2023,
+        "interval": 1}
     },
-    cluster_by = ["ano", "sigla_uf", "id_municipio"],
+    cluster_by = ["id_municipio", "sigla_uf"],
     labels = {'project_id': 'basedosdados'})
 }}
+
 SELECT
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(id_municipio AS STRING) id_municipio,
-SAFE_CAST(bairro_revenda AS STRING) bairro_revenda,
+INITCAP(bairro_revenda) AS bairro_revenda,
 SAFE_CAST(cep_revenda AS STRING) cep_revenda,
-SAFE_CAST(endereco_revenda AS STRING) endereco_revenda,
+INITCAP(endereco_revenda) AS endereco_revenda,
 SAFE_CAST(cnpj_revenda AS STRING) cnpj_revenda,
-SAFE_CAST(nome_estabelecimento AS STRING) nome_estabelecimento,
-SAFE_CAST(bandeira_revenda AS STRING) bandeira_revenda,
+INITCAP(nome_estabelecimento) AS  nome_estabelecimento,
+INITCAP(bandeira_revenda) AS bandeira_revenda,
 SAFE_CAST(data_coleta AS DATE) data_coleta,
-SAFE_CAST(produto AS STRING) produto,
+INITCAP(produto) AS produto,
 SAFE_CAST(unidade_medida AS STRING) unidade_medida,
 SAFE_CAST(preco_compra AS FLOAT64) preco_compra,
 SAFE_CAST(preco_venda AS FLOAT64) preco_venda
