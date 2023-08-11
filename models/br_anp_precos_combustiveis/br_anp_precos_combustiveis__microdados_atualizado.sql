9700ba0966a1aef945b9396853b2d75e68abddac
Patrick Teixeira <105399231+tricktx@users.noreply.github.com>
2023-08-11 14:05:15 -0300

    Update br_anp_precos_combustiveis__microdados_atualizado.sql

--- models/br_anp_precos_combustiveis/br_anp_precos_combustiveis__microdados_atualizado.sql
+++ models/br_anp_precos_combustiveis/br_anp_precos_combustiveis__microdados_atualizado.sql
@@ -11,7 +11,7 @@
         "interval": 1}
     },
     cluster_by = ["id_municipio", "sigla_uf"],
-    labels = {'project_id': 'basedosdados-dev'},
+    labels = {'project_id': 'basedosdados'},
     post_hook = ['REVOKE `roles/bigquery.dataViewer` ON TABLE {{ this }} FROM "specialGroup:allUsers"',
               'GRANT `roles/bigquery.dataViewer` ON TABLE {{ this }} TO "group:bd-pro@basedosdados.org"'])
 }}
