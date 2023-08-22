efd495f157b0c63df8cd6f1fd899693e8017c9b8
Pedro Castro <aspeddro@gmail.com>
2023-08-22 16:00:08 -0300

    fix table name

--- models/world_iea_pirls/world_iea_pirls__dictionary.sql
+++ models/world_iea_pirls/world_iea_pirls__dictionary.sql
@@ -6,4 +6,4 @@ SAFE_CAST(column_name AS STRING) column_name,
 SAFE_CAST(key AS STRING) key,
 SAFE_CAST(temporal_coverage AS STRING) temporal_coverage,
 SAFE_CAST(value AS STRING) value
-FROM basedosdados-staging.world_iea_pirls_staging.dicionario AS t
+FROM basedosdados-staging.world_iea_pirls_staging.dictionary AS t
