a16ed3e5fa07f3b150cbe17eb5e39af17f946141
folhesgabriel <pisagabriel09@gmail.com>
2023-08-22 19:18:01 -0300

    insere modelo dbt da tabela habilitacao

--- models/br_ms_cnes/habilitacao.sql
+++ models/br_ms_cnes/habilitacao.sql
@@ -25,7 +25,7 @@
 WITH raw_cnes_habilitacaol AS (
   -- 1. Retirar linhas com id_estabelecimento_cnes nulo
   SELECT *
-  FROM `basedosdados-staging.br_ms_cnes_staging.habilitacao`
+  FROM `basedosdados-dev.br_ms_cnes_staging.habilitacao`
   WHERE CNES IS NOT NULL
 ),
 raw_cnes_habilitacao_without_duplicates as(
@@ -34,11 +34,22 @@ raw_cnes_habilitacao_without_duplicates as(
   FROM raw_cnes_habilitacaol
 ),
 cnes_add_muni AS (
-  -- 3. Adicionar id_municipio e sigla_uf
-  SELECT *
+  -- 3. Adicionar id_municipio e sigla_uf e corrige erros de
+  --preenchimento na coluna CMPT_INI 
+  SELECT *,
+  CASE 
+    WHEN CMPT_INI = 'z/04de' THEN '2004' || SUBSTR(CMPT_INI, 3, 1) || '12'
+    WHEN CMPT_INI = 't/04se' THEN '2004' || SUBSTR(CMPT_INI, 3, 1) || '09'
+    WHEN CMPT_INI = 'n/05ja' THEN '2005' || SUBSTR(CMPT_INI, 3, 1) || '01'
+    WHEN CMPT_INI = 't/04se' THEN '2004' || SUBSTR(CMPT_INI, 3, 1) || '09'
+    WHEN CMPT_INI = 'n/05ju' THEN '2005' || SUBSTR(CMPT_INI, 3, 1) || '07'
+    WHEN CMPT_INI = 'o/04ag' THEN '2004' || SUBSTR(CMPT_INI, 3, 1) || '08'
+    WHEN CMPT_INI = 'l/05ju' THEN '2005' || SUBSTR(CMPT_INI, 3, 1) || '07'
+    ELSE CMPT_INI  
+  END AS CMPT_INI_padronized
   FROM raw_cnes_habilitacao_without_duplicates  
   LEFT JOIN (SELECT id_municipio, id_municipio_6,
-  FROM `basedosdados.br_bd_diretorios_brasil.municipio`) as mun
+  FROM `basedosdados-dev.br_bd_diretorios_brasil.municipio`) as mun
   ON raw_cnes_habilitacao_without_duplicates.CODUFMUN = mun.id_municipio_6
 )
 
@@ -49,8 +60,8 @@ SAFE_CAST(sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(id_municipio AS STRING) id_municipio,
 SAFE_CAST(CNES AS STRING) id_estabelecimento_cnes,
 SAFE_CAST(NULEITOS AS INT64) quantidade_leitos,
-CAST(SUBSTR(CMPT_INI, 1, 4) AS INT64) AS ano_competencia_inicial,
-CAST(SUBSTR(CMPT_INI, 5, 2) AS INT64) AS mes_competencia_inicial,
+CAST(SUBSTR(CMPT_INI_padronized, 1, 4) AS INT64) AS ano_competencia_inicial,
+CAST(SUBSTR(CMPT_INI_padronized, 5, 2) AS INT64) AS mes_competencia_inicial,
 CAST(SUBSTR(CMPT_FIM, 1, 4) AS INT64) AS ano_competencia_final,
 CAST(SUBSTR(CMPT_FIM, 5, 2) AS INT64) AS mes_competencia_final,
 SAFE_CAST(SGRUPHAB AS STRING) tipo_habilitacao,
