50741c5c0c3dc742a8563ddb92e7526370038e6d
Gabrielle Carvalho <77730866+gabrielle-carv@users.noreply.github.com>
2023-09-05 15:45:32 -0300

    tipo

--- models/br_bd_diretorios_brasil/regiao_metropolitana.sql
+++ models/br_bd_diretorios_brasil/regiao_metropolitana.sql
@@ -10,7 +10,7 @@ SAFE_CAST(lpad(id_recorte_metropolitano, 3, "0") AS STRING) id_recorte_metropoli
 SAFE_CAST(nome_recorte_metropolitano AS STRING) nome_recorte_metropolitano,
 SAFE_CAST(lpad(id_subcategoria_metropolitana, 7, "0") AS STRING) id_subcategoria_metropolitana,
 SAFE_CAST(nome_subcategoria_metropolitana AS STRING) nome_subcategoria_metropolitana,
-SAFE_CAST(tipo AS INT64) tipo,
+SAFE_CAST(tipo AS STRING) tipo,
 SAFE_CAST(sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(nome_regiao AS STRING) nome_regiao,
 FROM basedosdados-staging.br_bd_diretorios_brasil_staging.regiao_metropolitana AS t
