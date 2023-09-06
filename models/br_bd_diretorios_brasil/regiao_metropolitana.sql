5740a0168e58010d131937c1e9baced91092c273
Gabrielle Carvalho <77730866+gabrielle-carv@users.noreply.github.com>
2023-09-06 17:32:12 -0300

    att lista id_municipio

--- models/br_bd_diretorios_brasil/regiao_metropolitana.sql
+++ models/br_bd_diretorios_brasil/regiao_metropolitana.sql
@@ -4,13 +4,14 @@
     materialized='table',)
 }}
 SELECT 
-SAFE_CAST(lpad(id_regiao_metropolitana, 5, '0') AS STRING) id_regiao_metropolitana,
+SAFE_CAST(id_regiao_metropolitana AS STRING) id_regiao_metropolitana,
 SAFE_CAST(nome AS STRING) nome,
-SAFE_CAST(lpad(id_recorte_metropolitano, 3, "0") AS STRING) id_recorte_metropolitano,
+SAFE_CAST(id_recorte_metropolitano AS STRING) id_recorte_metropolitano,
 SAFE_CAST(nome_recorte_metropolitano AS STRING) nome_recorte_metropolitano,
-SAFE_CAST(lpad(id_subcategoria_metropolitana, 7, "0") AS STRING) id_subcategoria_metropolitana,
+SAFE_CAST(id_subcategoria_metropolitana AS STRING) id_subcategoria_metropolitana,
 SAFE_CAST(nome_subcategoria_metropolitana AS STRING) nome_subcategoria_metropolitana,
-SAFE_CAST(tipo AS STRING) tipo,
+SAFE_CAST(tipo AS string) tipo,
+SAFE_CAST(id_municipio AS STRING) id_municipio,
 SAFE_CAST(sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(nome_regiao AS STRING) nome_regiao,
 FROM basedosdados-staging.br_bd_diretorios_brasil_staging.regiao_metropolitana AS t
