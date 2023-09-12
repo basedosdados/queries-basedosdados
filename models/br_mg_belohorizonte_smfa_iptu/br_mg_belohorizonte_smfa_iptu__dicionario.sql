a849101a606a58456612f5c81bca7f30d7b1690a
tricktx <patrick.tx@hotmail.com.br>
2023-09-12 15:31:29 -0300

    replace dicionario

--- models/br_mg_belohorizonte_smfa_iptu/br_mg_belohorizonte_smfa_iptu__dicionario.sql
+++ models/br_mg_belohorizonte_smfa_iptu/br_mg_belohorizonte_smfa_iptu__dicionario.sql
@@ -6,5 +6,5 @@ SAFE_CAST(id_tabela AS STRING) id_tabela,
 SAFE_CAST(nome_coluna AS STRING) nome_coluna,
 SAFE_CAST(chave AS STRING) chave,
 SAFE_CAST(cobertura_temporal AS STRING) cobertura_temporal,
-SAFE_CAST(valor AS STRING) valor
+REPLACE(REPLACE(valor, '(Zona de Especial Interesse Social - 2', 'Zona de Especial Interesse Social - 2'), '(Zona de Preservação Ambiental', 'Zona de Preservação Ambiental') AS valor
 FROM basedosdados-staging.br_mg_belohorizonte_smfa_iptu_staging.dicionario AS t
