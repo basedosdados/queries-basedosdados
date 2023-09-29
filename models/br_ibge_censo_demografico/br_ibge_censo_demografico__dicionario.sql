78e35c86a9de791fa14c72382165ca492c7f8add
Gabrielle Carvalho <77730866+gabrielle-carv@users.noreply.github.com>
2023-09-29 14:39:35 -0300

    string

--- models/br_ibge_censo_demografico/br_ibge_censo_demografico__dicionario.sql
+++ models/br_ibge_censo_demografico/br_ibge_censo_demografico__dicionario.sql
@@ -8,7 +8,7 @@
 SELECT 
 SAFE_CAST(id_tabela AS STRING) id_tabela,
 SAFE_CAST(nome_coluna AS STRING) nome_coluna,
-SAFE_CAST(chave AS STRING) chave,
+SAFE_CAST(REPLACE(chave,".0","") AS STRING) chave,
 SAFE_CAST(cobertura_temporal AS STRING) cobertura_temporal,
 SAFE_CAST(valor AS STRING) valor
 FROM basedosdados-staging.br_ibge_censo_demografico_staging.dicionario AS t
