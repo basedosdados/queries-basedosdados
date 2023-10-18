485df1503beeb4b7ab55b11ba2ae4c509130fb77
Luiz Eduardo <luiz.sinx@gmail.com>
2023-10-18 14:39:33 -0300

    Update br_inpe_prodes_desmatamento_municipio.sql

--- models/br_inpe_prodes/br_inpe_prodes_desmatamento_municipio.sql
+++ models/br_inpe_prodes/br_inpe_prodes_desmatamento_municipio.sql
@@ -9,7 +9,7 @@ SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(id_municipio AS STRING) id_municipio,
 SAFE_CAST(bioma AS STRING) bioma,
 SAFE_CAST(area AS INT64) area,
-SAFE_CAST(desmatamento AS STRING) desmatamento,
+SAFE_CAST(desmatamento AS FLOAT64) desmatamento,
 SAFE_CAST(floresta AS FLOAT64) floresta,
 SAFE_CAST(nao_floresta AS FLOAT64) nao_floresta,
 SAFE_CAST(hidrografia AS FLOAT64) hidrografia
