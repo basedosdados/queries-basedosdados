fa706706a6c73df575be8e33e258ea69224fc54e
Pedro Castro <aspeddro@gmail.com>
2023-10-24 17:32:43 -0300

    increase month

--- models/br_cgu_servidores_executivo_federal/br_cgu_servidores_executivo_federal__cadastro_pensionistas.sql
+++ models/br_cgu_servidores_executivo_federal/br_cgu_servidores_executivo_federal__cadastro_pensionistas.sql
@@ -17,11 +17,11 @@
           'CREATE OR REPLACE ROW ACCESS POLICY allusers_filter
                       ON {{this}}
                       GRANT TO ("allUsers")
-                      FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano AS INT64),CAST(mes AS INT64),1), MONTH) > 6)',
+                      FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano AS INT64),CAST(mes AS INT64),1), MONTH) > 7)',
           'CREATE OR REPLACE ROW ACCESS POLICY bdpro_filter 
                       ON  {{this}}
                       GRANT TO ("group:bd-pro@basedosdados.org", "group:sudo@basedosdados.org")
-                    FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano AS INT64),CAST(mes AS INT64),1), MONTH) <= 6)'
+                    FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano AS INT64),CAST(mes AS INT64),1), MONTH) <= 7)'
         ]
     )
 }}
