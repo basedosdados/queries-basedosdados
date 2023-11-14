b1c102eb9075e84da3f320ac4b564ea81b5563e6
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-11-14 12:22:08 -0300

    fix: making requested changes

--- models/br_cgu_beneficios_cidadao/br_cgu_beneficios_cidadao__garantia_safra.sql
+++ models/br_cgu_beneficios_cidadao/br_cgu_beneficios_cidadao__garantia_safra.sql
@@ -4,33 +4,31 @@
     schema='br_cgu_beneficios_cidadao',
     materialized='incremental',
      partition_by={
-      "field": "ano",
+      "field": "ano_referencia",
       "data_type": "int64",
       "range": {
         "start": 2013,
         "end": 2024,
         "interval": 1}
     },
-    cluster_by = ["mes", "sigla_uf"] ,
+    cluster_by = ["mes_referencia", "sigla_uf"] ,
     pre_hook = "DROP ALL ROW ACCESS POLICIES ON {{ this }}",
     post_hook = [
         'CREATE OR REPLACE ROW ACCESS POLICY allusers_filter 
                     ON {{this}}
                     GRANT TO ("allUsers")
-                    FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano AS INT64),CAST(mes AS INT64),1), MONTH) > 6)',
+                    FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano_referencia AS INT64),CAST(mes_referencia AS INT64),1), MONTH) > 6)',
         'CREATE OR REPLACE ROW ACCESS POLICY bdpro_filter 
                     ON  {{this}}
                     GRANT TO ("group:bd-pro@basedosdados.org", "group:sudo@basedosdados.org")
-                    FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano AS INT64),CAST(mes AS INT64),1), MONTH) <= 6)']       )
+                    FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano_referencia AS INT64),CAST(mes_referencia AS INT64),1), MONTH) <= 6)']       )
 }}
 with garantia_safra as (
 SELECT 
-SAFE_CAST(SUBSTR(mes_referencia, 1, 4) AS INT64) ano,
-SAFE_CAST(SUBSTR(mes_referencia, 5, 2) AS INT64) mes,
-SAFE_CAST(mes_referencia AS STRING) data_referencia,
+SAFE_CAST(SUBSTR(mes_referencia, 1, 4) AS INT64) ano_referencia,
+SAFE_CAST(SUBSTR(mes_referencia, 5, 2) AS INT64) mes_referencia,
 SAFE_CAST(PARSE_DATE('%Y%m',mes_referencia) AS DATE) data,
 t2.id_municipio,
-t2.nome as nome_municipio,
 SAFE_CAST(t1.sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(nis AS STRING) nis_favorecido,
 SAFE_CAST(t1.nome AS STRING) nome_favorecido,
--- models/br_cgu_beneficios_cidadao/br_cgu_beneficios_cidadao__novo_bolsa_familia.sql
+++ models/br_cgu_beneficios_cidadao/br_cgu_beneficios_cidadao__novo_bolsa_familia.sql
@@ -4,33 +4,33 @@
     schema='br_cgu_beneficios_cidadao',
     materialized='incremental',
      partition_by={
-      "field": "ano",
+      "field": "ano_competencia",
       "data_type": "int64",
       "range": {
         "start": 2023,
         "end": 2024,
         "interval": 1}
     },
-    cluster_by = ["mes", "sigla_uf"],
+    cluster_by = ["mes_competencia", "sigla_uf"],
     pre_hook = "DROP ALL ROW ACCESS POLICIES ON {{ this }}",
     post_hook = [
         'CREATE OR REPLACE ROW ACCESS POLICY allusers_filter 
                     ON {{this}}
                     GRANT TO ("allUsers")
-                    FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano AS INT64),CAST(mes AS INT64),1), MONTH) > 6)',
+                    FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano_competencia AS INT64),CAST(mes_competencia AS INT64),1), MONTH) > 6)',
         'CREATE OR REPLACE ROW ACCESS POLICY bdpro_filter 
                     ON  {{this}}
                     GRANT TO ("group:bd-pro@basedosdados.org", "group:sudo@basedosdados.org")
-                    FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano AS INT64),CAST(mes AS INT64),1), MONTH) <= 6)']      )
+                    FILTER USING (DATE_DIFF(DATE("{{ run_started_at.strftime("%Y-%m-%d") }}"),DATE(CAST(ano_competencia AS INT64),CAST(mes_competencia AS INT64),1), MONTH) <= 6)']      )
 }}
 with novo_bolsa_familia as (
 SELECT 
-SAFE_CAST(SUBSTR(mes_competencia, 1, 4) AS INT64) ano,
-SAFE_CAST(SUBSTR(mes_competencia, 5, 2) AS INT64) mes,
-SAFE_CAST(mes_referencia AS STRING) data_referencia,
+SAFE_CAST(SUBSTR(mes_competencia, 1, 4) AS INT64) ano_competencia,
+SAFE_CAST(SUBSTR(mes_competencia, 5, 2) AS INT64) mes_competencia,
+SAFE_CAST(SUBSTR(mes_referencia, 1, 4) AS INT64) ano_referencia,
+SAFE_CAST(SUBSTR(mes_referencia, 5, 2) AS INT64) mes_referencia,
 SAFE_CAST(PARSE_DATE('%Y%m',mes_referencia) AS DATE) data,
 t2.id_municipio,
-t2.nome as nome_municipio,
 SAFE_CAST(t1.sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(cpf AS STRING) cpf_favorecido,
 SAFE_CAST(nis AS STRING) nis_favorecido,
