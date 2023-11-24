12ef6575c83470f7fc9421e06cf169744ace0b72
folhesgabriel <pisagabriel09@gmail.com>
2023-11-24 10:39:10 -0300

    corrige typo no modelo

--- models/br_trase_supply_chain/br_trase_supply_chain__soy_beans_refining_facilities.sql
+++ models/br_trase_supply_chain/br_trase_supply_chain__soy_beans_refining_facilities.sql
@@ -11,8 +11,7 @@ SAFE_CAST(cartodb_id AS STRING) cartodb_id,
 SAFE_CAST(the_geom_webmercator AS STRING) geom_webmercator_id,
 SAFE_CAST(geocode AS STRING) municipality_id,
 SAFE_CAST(state AS STRING) state,
-SAFE_CAST(REPLACE(capacity, 'NA', '') AS INT64) capacity,
 SAFE_CAST(company AS STRING) company,
-SAFE_CAST(capacity AS INT64) capacity,
+SAFE_CAST(REPLACE(capacity, 'NA', '') AS INT64) capacity,
 SAFE_CAST(ST_GEOGPOINT(SAFE_CAST(long AS FLOAT64),SAFE_CAST(lat AS FLOAT64)) as GEOGRAPHY) point
 FROM basedosdados-staging.br_trase_supply_chain_staging.soy_beans_refining_facilities AS t
