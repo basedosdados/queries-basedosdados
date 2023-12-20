ff209903853cbceaa5afcf2f8a484ac9cb16a68c
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-12-20 16:12:02 -0300

    feat(schema): changing column name in schema

--- models/br_ibge_censo_2022/br_ibge_censo_2022__quilombolas_domicilios_moradores_municipio.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__quilombolas_domicilios_moradores_municipio.sql
@@ -7,12 +7,11 @@ SAFE_CAST(TRIM(REGEXP_EXTRACT(municipio, r'\(([^)]+)\)')) AS STRING) sigla_uf,
 SAFE_CAST(domicilios_particulares_permanentes_ocupados_domicilios_ AS INT64) domicilios,
 SAFE_CAST(moradores_em_domicilios_particulares_permanentes_ocupados_pessoas_ AS INT64) moradores_domicilios,
 SAFE_CAST(moradores_quilombolas_em_domicilios_particulares_permanentes_ocupados_pessoas_ AS INT64) moradores_quilombolas_domicilios,
-SAFE_CAST(REPLACE(media_de_moradores_em_domicilios_particulares_permanentes_ocupados_pessoas_, ",", ".") AS FLOAT64) media_moradres_domicilios,
-SAFE_CAST(REPLACE(media_de_moradores_quilombolas_em_domicilios_particulares_permanentes_ocupados_com_pelo_menos_um_morador_quilombola_pessoas_, ",", ".") AS FLOAT64) media_moradores_quilombolas_domicilios,
+#SAFE_CAST(REPLACE(media_de_moradores_em_domicilios_particulares_permanentes_ocupados_pessoas_, ",", ".") AS FLOAT64) media_moradres_domicilios,
+#SAFE_CAST(REPLACE(media_de_moradores_quilombolas_em_domicilios_particulares_permanentes_ocupados_com_pelo_menos_um_morador_quilombola_pessoas_, ",", ".") AS FLOAT64) media_moradores_quilombolas_domicilios,
 SAFE_CAST(localizacao_do_domicilio AS STRING) localizacao_domicilio,
 FROM basedosdados-dev.br_ibge_censo_2022_staging.quilombolas_domicilios_moradores_municipio AS t)
 select t2.cod as id_municipio, ibge.* except(municipio, nome_municipio,sigla_uf) 
 from ibge
 left join `basedosdados-dev.br_ibge_censo_2022_staging.auxiliary_table` t2
 on ibge.municipio = t2.municipio
-
