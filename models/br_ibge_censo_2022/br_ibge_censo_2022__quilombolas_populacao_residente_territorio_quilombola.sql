84ff3284bb01ce63a8777a2be091bd5aab1c945d
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-12-22 17:58:23 -0300

    fix(queries): fixing bucket names

--- models/br_ibge_censo_2022/br_ibge_censo_2022__quilombolas_populacao_residente_territorio_quilombola.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__quilombolas_populacao_residente_territorio_quilombola.sql
@@ -4,7 +4,7 @@ SAFE_CAST(TRIM(REGEXP_EXTRACT(territorio_quilombola_por_unidade_da_federacao, r'
 SAFE_CAST(TRIM(REGEXP_EXTRACT(territorio_quilombola_por_unidade_da_federacao, r'\(([^)]+)\)')) AS STRING) sigla_uf,
 SAFE_CAST(pessoas_quilombolas_residentes_em_territorios_quilombolas_pessoas_ AS INT64) pessoas_quilombolas,
 SAFE_CAST(pessoas_residentes_em_territorios_quilombolas_pessoas_ AS INT64) populacao_residente,
-FROM basedosdados-staging.br_ibge_censo_2022_staging.populacao_residente_territorios_quilombolas AS t
+FROM basedosdados-staging.br_ibge_censo_2022_staging.quilombolas_populacao_residente_territorio_quilombola AS t
 
 
 
