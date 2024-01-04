7fdb6dd52b227d389b5d75d8bdf39cf9020954b6
Winzen <luiz.sinx@gmail.com>
2024-01-04 11:16:53 -0300

    Adição da table violencia_escola a br_fbsp_absp

---
+++ models/br_fbsp_absp/br_fbsp_absp__violencia_escola.sql
@@ -0,0 +1,13 @@
+{
+    config(
+        alias='violencia_escola',
+        schema='br_fbsp_absp'
+    )
+}}
+SELECT
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(uf AS STRING) uf,
+SAFE_CAST(tema AS STRING) tema,
+SAFE_CAST(item AS STRING) item,
+SAFE_CAST(quantidade_escola AS FLOAT64) quantidade_escola
+FROM basedosdados-staging.br_fbsp_absp_staging.violencia_escola AS t
