47fa866b7ed8b61374972e1ca82cdbae95d433f8
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-01-30 19:01:58 -0300

    feat(pib): updating br_ibge_pib.municipio

---
+++ models/br_ibge_pib/br_ibge_pib__municipio.sql
@@ -0,0 +1,13 @@
+{{ config(alias='municipio',schema='br_ibge_pib') }}
+SELECT 
+SAFE_CAST(id_municipio AS STRING) id_municipio,
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(pib AS INT64) pib,
+SAFE_CAST(impostos_liquidos AS INT64) impostos_liquidos,
+SAFE_CAST(va AS INT64) va,
+SAFE_CAST(va_agropecuaria AS INT64) va_agropecuaria,
+SAFE_CAST(va_industria AS INT64) va_industria,
+SAFE_CAST(va_servicos AS INT64) va_servicos,
+SAFE_CAST(va_adespss AS INT64) va_adespss
+FROM basedosdados-dev.br_ibge_pib_staging.municipio AS t
+
