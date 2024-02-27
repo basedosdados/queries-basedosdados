0367fb3a8542f844ceff5100e73a0b751b4eb87b
folhesgabriel <pisagabriel09@gmail.com>
2024-02-26 22:09:28 -0300

    fix: temporary remove pre hook to materialize table

--- models/br_ms_cnes/br_ms_cnes__estabelecimento.sql
+++ models/br_ms_cnes/br_ms_cnes__estabelecimento.sql
@@ -8,11 +8,6 @@
             "data_type": "int64",
             "range": {"start": 2005, "end": 2024, "interval": 1},
         },
-        pre_hook="DROP ALL ROW ACCESS POLICIES ON {{ this }}",
-        post_hook=[
-            'CREATE OR REPLACE ROW ACCESS POLICY allusers_filter                     ON {{this}}                     GRANT TO ("allUsers")                     FILTER USING (DATE_DIFF(CURRENT_DATE(),DATE(CAST(ano AS INT64),CAST(mes AS INT64),1), MONTH) > 6)',
-            'CREATE OR REPLACE ROW ACCESS POLICY bdpro_filter        ON  {{this}}                     GRANT TO ("group:bd-pro@basedosdados.org", "group:sudo@basedosdados.org")                     FILTER USING (True)',
-        ],
     )
 }}
 with
@@ -272,7 +267,6 @@ select
 from cnes_add_muni
 {% if is_incremental() %}
     where
-
         date(cast(ano as int64), cast(mes as int64), 1)
         > (select max(date(cast(ano as int64), cast(mes as int64), 1)) from {{ this }})
 {% endif %}
