58d5a028cf5623147489707bf4b543b16f5c1861
tricktx <patrick.tx@hotmail.com.br>
2024-03-06 19:50:07 -0300

    add model dbt table despesa

--- models/br_camara_dados_abertos/br_camara_dados_abertos__frente_deputado.sql
+++ models/br_camara_dados_abertos/br_camara_dados_abertos__frente_deputado.sql
@@ -1,7 +1,7 @@
 {{ config(alias="frente_deputado", schema="br_camara_dados_abertos") }}
 select distinct
     safe_cast(id as string) id_frente,
-    safe_cast(titulo as string) titulo,
+    safe_cast(titulo as string) titulo_deputado,
     safe_cast(replace(id_deputado, ".0", "") as string) id_deputado,
     initcap(nome_deputado) nome_deputado,
     safe_cast(titulo_deputado as string) titulo_deputado,
