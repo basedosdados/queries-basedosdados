62b81763564baf0006baa270892073879b747859
tricktx <patrick.tx@hotmail.com.br>
2024-03-22 13:02:37 -0300

    fix colum numero_consumidores

--- models/br_mme_consumo_energia_eletrica/br_mme_consumo_energia_eletrica__uf.sql
+++ models/br_mme_consumo_energia_eletrica/br_mme_consumo_energia_eletrica__uf.sql
@@ -6,10 +6,14 @@
     )
 }}
 select
-    safe_cast(ano as int64) ano,
-    safe_cast(mes as int64) mes,
-    safe_cast(sigla_uf as string) sigla_uf,
-    safe_cast(tipo_consumo as string) tipo_consumo,
-    safe_cast(numero_consumidores as float64) numero_consumidores,
-    safe_cast(consumo as float64) consumo
+    safe_cast(ano as int64) as ano,
+    safe_cast(mes as int64) as mes,
+    safe_cast(sigla_uf as string) as sigla_uf,
+    safe_cast(tipo_consumo as string) as tipo_consumo,
+    case
+        when numero_consumidores = '0'
+        then null
+        else safe_cast(numero_consumidores as int64)
+    end as numero_consumidores,
+    safe_cast(consumo as int64) as consumo
 from `basedosdados-staging.br_mme_consumo_energia_eletrica_staging.uf` as t
