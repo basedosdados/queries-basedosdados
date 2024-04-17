5f9ee9c13828f7272a7dab8b924bb71d6b0a8476
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-04-17 11:17:25 -0300

    fix: fix project name

--- models/br_cgu_beneficios_cidadao/br_cgu_beneficios_cidadao__novo_bolsa_familia.sql
+++ models/br_cgu_beneficios_cidadao/br_cgu_beneficios_cidadao__novo_bolsa_familia.sql
@@ -16,7 +16,7 @@
         ],
     )
 }}
-select 
+select
     safe_cast(substr(mes_competencia, 1, 4) as int64) ano_competencia,
     safe_cast(substr(mes_competencia, 5, 2) as int64) mes_competencia,
     safe_cast(substr(mes_referencia, 1, 4) as int64) ano_referencia,
@@ -27,7 +27,7 @@ select
     safe_cast(nis as string) nis_favorecido,
     safe_cast(t1.nome as string) nome_favorecido,
     safe_cast(valor as float64) valor_parcela,
-from `basedosdados-dev.br_cgu_beneficios_cidadao_staging.novo_bolsa_familia` t1
+from `basedosdados-staging.br_cgu_beneficios_cidadao_staging.novo_bolsa_familia` t1
 left join
     `basedosdados.br_bd_diretorios_brasil.municipio` t2
     on safe_cast(t1.id_municipio_siafi as int64)
