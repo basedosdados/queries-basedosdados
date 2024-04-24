6916901126b4e9db6382dc405945e8af91e71dbd
folhesgabriel <pisagabriel09@gmail.com>
2024-04-24 17:01:08 -0300

    minor fixes

--- models/br_me_comex_stat/br_me_comex_stat__dicionario.sql
+++ models/br_me_comex_stat/br_me_comex_stat__dicionario.sql
@@ -6,14 +6,14 @@
 }}
 select
     safe_cast(id_tabela as string) id_tabela,
+    safe_cast(coluna as string) nome_coluna,
     safe_cast(
         case
             when coluna = 'id_urf'
             then lpad(safe_cast(chave as string), 6, '0')
             else safe_cast(chave as string)
         end as string
-    ) nome_coluna,
-    safe_cast(chave as string) chave,
+    ) chave,
     safe_cast(cobertura_temporal as string) cobertura_temporal,
     safe_cast(valor as string) valor
-from `basedosdados-staging.br_me_comex_stat_staging.dicionario` as t
+from `basedosdados-dev.br_me_comex_stat_staging.dicionario` as t
