d636e0818bad0bcb996c9e80c300b2c11f226711
Jean Tozzi <jeancnstozzi@gmail.com>
2024-05-03 10:52:40 -0300

    add: br_ibge_ppm models and files

---
+++ models/br_ibge_ppm/br_ibge_ppm__efetivo_rebanhos.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        alias="efetivo_rebanhos",
+        schema="br_ibge_ppm",
+        partition_by={
+            "field": "ano",
+            "data_type": "int64",
+            "range": {"start": 1974, "end": 2022, "interval": 1},
+        },
+        cluster_by=["sigla_uf", "id_municipio"],
+    )
+}}
+select
+    safe_cast(ano as int64) ano,
+    safe_cast(sigla_uf as string) sigla_uf,
+    safe_cast(id_municipio as string) id_municipio,
+    safe_cast(tipo_rebanho as string) tipo_rebanho,
+    safe_cast(quantidade as int64) quantidade
+from `basedosdados-staging.br_ibge_ppm_staging.efetivo_rebanhos` as t
+where quantidade is not null
+
