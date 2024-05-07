27135e94efed1e1e097ea5036bab7616d44cea11
Laura Amaral <laura.amaral@basedosdados.org>
2024-05-07 14:33:27 -0300

    fix: remove deleted dataset

--- models/br_camara_atividade_legislativa/br_camara_atividade_legislativa__dicionario.sql
+++
@@ -1,8 +0,0 @@
-{{ config(alias="dicionario", schema="br_camara_atividade_legislativa") }}
-select
-    safe_cast(id_tabela as string) id_tabela,
-    safe_cast(nome_coluna as string) nome_coluna,
-    safe_cast(chave as string) chave,
-    safe_cast(cobertura_temporal as string) cobertura_temporal,
-    safe_cast(valor as string) valor,
-from `basedosdados-staging.br_camara_atividade_legislativa_staging.dicionario` as t
