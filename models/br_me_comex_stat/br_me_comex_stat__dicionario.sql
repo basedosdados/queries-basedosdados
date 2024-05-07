f8c7c9fdbe4983d90d2de21ba6585e05fff89e8e
Laura Amaral <laura.amaral@basedosdados.org>
2024-05-07 14:52:21 -0300

    fix: comex_stat dictionary

--- models/br_me_comex_stat/br_me_comex_stat__dicionario.sql
+++ models/br_me_comex_stat/br_me_comex_stat__dicionario.sql
@@ -2,7 +2,13 @@
 select
     safe_cast(id_tabela as string) id_tabela,
     safe_cast(coluna as string) nome_coluna,
-    safe_cast(chave as string) chave,
+    safe_cast(
+        case
+            when coluna = 'id_urf'
+            then lpad(safe_cast(chave as string), 6, '0')
+            else safe_cast(chave as string)
+        end as string
+    ) chave,
     safe_cast(cobertura_temporal as string) cobertura_temporal,
     safe_cast(valor as string) valor,
 from `basedosdados-staging.br_me_comex_stat_staging.dicionario` as t
