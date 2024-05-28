0c7761e32d85ce10d972ef5037088520e751cc4c
tricktx <patrick.tx@hotmail.com.br>
2024-05-28 10:40:49 -0300

    table approve in dicionario rais

--- models/br_me_rais/br_me_rais__dicionario.sql
+++ models/br_me_rais/br_me_rais__dicionario.sql
@@ -3,6 +3,6 @@ select
     safe_cast(id_tabela as string) id_tabela,
     safe_cast(nome_coluna as string) nome_coluna,
     safe_cast(chave as string) chave,
-    safe_cast(cobertura_temporal as string) cobertura_temporal,
+    safe_cast(replace(cobertura_temporal, '-1', '(1)') as string) cobertura_temporal,
     safe_cast(valor as string) valor,
 from `basedosdados-staging.br_me_rais_staging.dicionario` as t
