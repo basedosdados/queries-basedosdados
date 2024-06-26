989c98dc2189c2ebafcccf711e9e863055506076
uiro-bi <pisagabriel09@gmail.com>
2024-06-26 11:00:40 -0300

    feat: update cnae_2 and natureza_juridica diretórios

--- models/br_bd_diretorios_brasil/br_bd_diretorios_brasil__cnae_2.sql
+++ models/br_bd_diretorios_brasil/br_bd_diretorios_brasil__cnae_2.sql
@@ -6,12 +6,14 @@
     )
 }}
 select
-    safe_cast(replace(replace(cnae_2, '.', ''), '-', '') as string) as cnae_2,
-    safe_cast(descricao as string) as descricao,
-    safe_cast(grupo as string) as grupo,
-    safe_cast(descricao_grupo as string) as descricao_grupo,
+    safe_cast(secao as string) as secao,
+    safe_cast(descricao_secao as string) as descricao_secao,
     safe_cast(divisao as string) as divisao,
     safe_cast(descricao_divisao as string) as descricao_divisao,
-    safe_cast(secao as string) as secao,
-    safe_cast(descricao_secao as string) as descricao_secao
+    safe_cast(grupo as string) as grupo,
+    safe_cast(descricao_grupo as string) as descricao_grupo,
+    safe_cast(classe as string) as classe,
+    safe_cast(descricao_classe as string) as descricao_classe,
+    safe_cast(subclasse as string) as subclasse,
+    safe_cast(descricao_subclasse as string) as descricao_subclasse
 from `basedosdados-staging.br_bd_diretorios_brasil_staging.cnae_2` as t
