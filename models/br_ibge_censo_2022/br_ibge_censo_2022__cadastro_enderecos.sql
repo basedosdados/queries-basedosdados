629080d2b8726abf88fd9f33db4510db7ad42e10
uiro-bi <pisagabriel09@gmail.com>
2024-07-08 12:11:28 -0300

    fix: not null tests in schema.yml

--- models/br_ibge_censo_2022/br_ibge_censo_2022__cadastro_enderecos.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__cadastro_enderecos.sql
@@ -45,5 +45,5 @@ select
     safe_cast(nv_geo_coord as string) nivel_geocodificacao_coordenadas,
     safe_cast(latitude as string) latitude,
     safe_cast(longitude as string) longitude,
-    st_geogpoint(safe_cast(longitude as float64), safe_cast(latitude as float64)) ponto,
+    st_geogpoint(safe_cast(longitude as float64), safe_cast(latitude as float64)) ponto
 from `basedosdados-staging.br_ibge_censo_2022_staging.cadastro_enderecos` as t
