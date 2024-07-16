634b3324cee3344ce7715f97f849a64875527c1c
uiro-bi <pisagabriel09@gmail.com>
2024-07-16 16:08:05 -0300

    feat: re insert standart incremental logic

--- models/br_ms_sih/br_ms_sih__aihs_reduzidas.sql
+++ models/br_ms_sih/br_ms_sih__aihs_reduzidas.sql
@@ -326,5 +326,9 @@ select
     safe_cast(val_uti as float64) valor_uti,
     safe_cast(us_tot as float64) valor_dolar,
     safe_cast(val_tot as float64) valor_aih,
-from `basedosdados-staging.br_ms_sih_staging.aihs_reduzidas` as t
-{% if is_incremental() %} where cast(ano as int64) = 2024 {% endif %}
+from
+    `basedosdados-staging.br_ms_sih_staging.aihs_reduzidas` as t
+    {% if is_incremental() %}
+        date(cast(ano as int64), cast(mes as int64), 1)
+        > (select max(date(cast(ano as int64), cast(mes as int64), 1)) from {{ this }})
+    {% endif %}
