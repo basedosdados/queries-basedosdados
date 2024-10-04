721729027889e3078b1d25bcfbf242706fc2e612
andre-ls <andre.luis.e3@gmail.com>
2024-10-04 17:09:22 +0000

    Execução de Pre-Commit nas Queries

--- models/br_rf_arrecadacao/br_rf_arrecadacao__ir_ipi.sql
+++ models/br_rf_arrecadacao/br_rf_arrecadacao__ir_ipi.sql
@@ -1,14 +1,13 @@
-{{ config(alias='ir_ipi',schema='br_rf_arrecadacao') }}
+{{ config(alias="ir_ipi", schema="br_rf_arrecadacao") }}
 select
-safe_cast(ano as int64) ano,
-safe_cast(mes as int64) mes,
-safe_cast(tributo as string) tributo,
-safe_cast(decendio as string) decendio,
-safe_cast(arrecadacao_bruta as float64) arrecadacao_bruta,
-safe_cast(retificacao as float64) retificacao,
-safe_cast(compensacao as float64) compensacao,
-safe_cast(restituicao as float64) restituicao,
-safe_cast(outros as float64) outros,
-safe_cast(arrecadacao_liquida as float64) arrecadacao_liquida,
+    safe_cast(ano as int64) ano,
+    safe_cast(mes as int64) mes,
+    safe_cast(tributo as string) tributo,
+    safe_cast(decendio as string) decendio,
+    safe_cast(arrecadacao_bruta as float64) arrecadacao_bruta,
+    safe_cast(retificacao as float64) retificacao,
+    safe_cast(compensacao as float64) compensacao,
+    safe_cast(restituicao as float64) restituicao,
+    safe_cast(outros as float64) outros,
+    safe_cast(arrecadacao_liquida as float64) arrecadacao_liquida,
 from `basedosdados-staging.br_rf_arrecadacao_staging.ir_ipi` as t
-
--- models/br_rf_arrecadacao/br_rf_arrecadacao__itr.sql
+++ models/br_rf_arrecadacao/br_rf_arrecadacao__itr.sql
@@ -1,10 +1,9 @@
-{{ config(alias='itr',schema='br_rf_arrecadacao') }}
+{{ config(alias="itr", schema="br_rf_arrecadacao") }}
 select
-safe_cast(ano as int64) ano,
-safe_cast(mes as int64) mes,
-safe_cast(nome_uf as string) nome_uf,
-safe_cast(regiao_politica as string) regiao_politica,
-safe_cast(cidade_uf as string) cidade_uf,
-safe_cast(valor_arrecadado as float64) valor_arrecadado,
+    safe_cast(ano as int64) ano,
+    safe_cast(mes as int64) mes,
+    safe_cast(nome_uf as string) nome_uf,
+    safe_cast(regiao_politica as string) regiao_politica,
+    safe_cast(cidade_uf as string) cidade_uf,
+    safe_cast(valor_arrecadado as float64) valor_arrecadado,
 from `basedosdados-staging.br_rf_arrecadacao_staging.itr` as t
-
