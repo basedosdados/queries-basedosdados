76a7d3fff345013e982ba4f62756aae8642a814c
Luiz Eduardo <luiz.sinx02@gmail.com>
2024-10-28 10:56:12 -0300

    Adicionar br_tse_eleicoes__resultados_partido_municipio e testes

--- models/br_tse_eleicoes/br_tse_eleicoes__resultados_partido_municipio.sql
+++ models/br_tse_eleicoes/br_tse_eleicoes__resultados_partido_municipio.sql
@@ -6,7 +6,7 @@
         partition_by={
             "field": "ano",
             "data_type": "int64",
-            "range": {"start": 1994, "end": 2022, "interval": 2},
+            "range": {"start": 1994, "end": 2024, "interval": 2},
         },
         cluster_by=["sigla_uf"],
     )
@@ -17,7 +17,7 @@ select
     safe_cast(turno as int64) turno,
     safe_cast(id_eleicao as string) id_eleicao,
     safe_cast(tipo_eleicao as string) tipo_eleicao,
-    safe_cast(data_eleicao as string) data_eleicao,
+    safe_cast(data_eleicao as date) data_eleicao,
     safe_cast(sigla_uf as string) sigla_uf,
     safe_cast(id_municipio as string) id_municipio,
     safe_cast(id_municipio_tse as string) id_municipio_tse,
@@ -26,4 +26,4 @@ select
     safe_cast(sigla_partido as string) sigla_partido,
     safe_cast(votos_nominais as int64) votos_nominais,
     safe_cast(votos_legenda as int64) votos_legenda
-from `basedosdados-staging.br_tse_eleicoes_staging.resultados_partido_municipio` as t
+from `basedosdados-dev.br_tse_eleicoes_staging.resultados_partido_municipio` as t
