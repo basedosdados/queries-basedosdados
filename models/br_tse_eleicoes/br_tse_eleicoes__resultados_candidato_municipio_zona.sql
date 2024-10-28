7dcbec382442c126ae6fe70a2f4e1f2b50946312
Luiz Eduardo <luiz.sinx02@gmail.com>
2024-10-28 10:37:11 -0300

    Adicionar modelo resultados_candidato_municipio_zona e testes

--- models/br_tse_eleicoes/br_tse_eleicoes__resultados_candidato_municipio_zona.sql
+++ models/br_tse_eleicoes/br_tse_eleicoes__resultados_candidato_municipio_zona.sql
@@ -6,7 +6,7 @@
         partition_by={
             "field": "ano",
             "data_type": "int64",
-            "range": {"start": 1998, "end": 2022, "interval": 2},
+            "range": {"start": 1994, "end": 2024, "interval": 2},
         },
         cluster_by=["sigla_uf"],
     )
@@ -16,7 +16,7 @@ select
     safe_cast(turno as int64) turno,
     safe_cast(id_eleicao as string) id_eleicao,
     safe_cast(tipo_eleicao as string) tipo_eleicao,
-    safe_cast(data_eleicao as string) data_eleicao,
+    safe_cast(data_eleicao as date) data_eleicao,
     safe_cast(sigla_uf as string) sigla_uf,
     safe_cast(id_municipio as string) id_municipio,
     safe_cast(id_municipio_tse as string) id_municipio_tse,
@@ -30,5 +30,5 @@ select
     safe_cast(resultado as string) resultado,
     safe_cast(votos as int64) votos
 from
-    `basedosdados-staging.br_tse_eleicoes_staging.resultados_candidato_municipio_zona`
+    `basedosdados-stating.br_tse_eleicoes_staging.resultados_candidato_municipio_zona`
     as t
