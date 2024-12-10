7f24a90e046e39e8a3318f9ed03872640ed2b83d
folhesgabriel <pisagabriel09@gmail.com>
2024-12-10 18:06:41 -0300

    trigger table approve

--- models/br_bd_diretorios_mundo/br_bd_diretorios_mundo__sistema_harmonizado.sql
+++ models/br_bd_diretorios_mundo/br_bd_diretorios_mundo__sistema_harmonizado.sql
@@ -18,4 +18,4 @@ select
     safe_cast(no_sh4_ing as string) nome_sh4_ingles,
     safe_cast(no_sh2_ing as string) nome_sh2_ingles,
     safe_cast(no_sec_ing as string) nome_sec_ingles,
-from `basedosdados-staging.br_bd_diretorios_mundo_staging.sistema_harmonizado` as t
+from `basedosdados-staging.br_bd_diretorios_mundo_staging.sistema_harmonizado` as x
