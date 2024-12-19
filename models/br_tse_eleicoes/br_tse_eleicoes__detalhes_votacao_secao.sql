8e96d785727447ee55eb64aebd0382ce220583e3
Luiz Eduardo <luiz.sinx02@gmail.com>
2024-12-19 11:01:16 -0300

    Adicionar id_eleicao e data_eleicao a detalhes_votacao_secao

--- models/br_tse_eleicoes/br_tse_eleicoes__detalhes_votacao_secao.sql
+++ models/br_tse_eleicoes/br_tse_eleicoes__detalhes_votacao_secao.sql
@@ -15,7 +15,9 @@
 select
     safe_cast(ano as int64) ano,
     safe_cast(turno as int64) turno,
+    safe_cast(id_eleicao as string) id_eleicao,
     safe_cast(tipo_eleicao as string) tipo_eleicao,
+    safe_cast(data_eleicao as date) data_eleicao,
     safe_cast(sigla_uf as string) sigla_uf,
     safe_cast(id_municipio as string) id_municipio,
     safe_cast(id_municipio_tse as string) id_municipio_tse,
@@ -28,12 +30,11 @@ select
     safe_cast(votos_nominais as int64) votos_nominais,
     safe_cast(votos_brancos as int64) votos_brancos,
     safe_cast(votos_nulos as int64) votos_nulos,
-    safe_cast(votos_coligacao as int64) votos_coligacao,
+    safe_cast(votos_legenda as int64) votos_legenda,
     safe_cast(votos_nulos_apu_sep as int64) votos_nulos_apu_sep,
-    safe_cast(votos_pendentes as int64) votos_pendentes,
     safe_cast(proporcao_comparecimento as float64) proporcao_comparecimento,
     safe_cast(proporcao_votos_nominais as float64) proporcao_votos_nominais,
-    safe_cast(proporcao_votos_coligacao as float64) proporcao_votos_coligacao,
+    safe_cast(proporcao_votos_legenda as float64) proporcao_votos_legenda,
     safe_cast(proporcao_votos_brancos as float64) proporcao_votos_brancos,
     safe_cast(proporcao_votos_nulos as float64) proporcao_votos_nulos
 from `basedosdados-staging.br_tse_eleicoes_staging.detalhes_votacao_secao` as t
