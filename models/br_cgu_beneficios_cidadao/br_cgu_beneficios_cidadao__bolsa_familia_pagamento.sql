7c05bdd0fa4357106227a7945d66a383a5d4da7c
tricktx <patrick.tx@hotmail.com.br>
2025-01-07 14:17:03 -0300

    register flow

--- models/br_cgu_beneficios_cidadao/br_cgu_beneficios_cidadao__bolsa_familia_pagamento.sql
+++ models/br_cgu_beneficios_cidadao/br_cgu_beneficios_cidadao__bolsa_familia_pagamento.sql
@@ -16,7 +16,6 @@
     )
 }}
 
-
 select distinct
     safe_cast(split(mes_ref, '-')[offset(0)] as int64) as ano_competencia,
     safe_cast(split(mes_ref, '-')[offset(1)] as int64) as mes_competencia,
