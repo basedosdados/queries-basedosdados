0238f151d37662568508ae71b92c6929624df74b
tricktx <patrick.tx@hotmail.com.br>
2025-01-07 11:22:24 -0300

    pass pre commit lint

--- models/br_cgu_beneficios_cidadao/br_cgu_beneficios_cidadao__bolsa_familia_pagamento.sql
+++ models/br_cgu_beneficios_cidadao/br_cgu_beneficios_cidadao__bolsa_familia_pagamento.sql
@@ -16,6 +16,7 @@
     )
 }}
 
+
 select distinct
     safe_cast(split(mes_ref, '-')[offset(0)] as int64) as ano_competencia,
     safe_cast(split(mes_ref, '-')[offset(1)] as int64) as mes_competencia,
